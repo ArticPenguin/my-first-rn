ad3388d2b3ffdeed3b95ec3575b08399c5fe7c15
민욱 <fast1597@gmail.com>
2024-08-31 12:04:17 +0900

    핸즈온RN - (1) 리액트 네이티브 시작하기

--- App.js
+++ App.js
@@ -2,9 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Expo React Native');
+
+  const name = 'beomjune';
+  const isFullname = true;
+
+  const add = (a, b) => {
+    return a + b;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}
+    // 여는 태그 주석
+    >
+      {/* 일반 주석 */}
+      <Text>My name is {name}</Text>
+      <Text>1 + 2 = {add(1, 2)}</Text>
+      <Text>{isFullname === true ? name + ' kim' : name}</Text>
       <StatusBar style="auto" />
     </View>
   );
